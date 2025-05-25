76c1399a02209255afd52454c48a8f7020580d9c
Vemy <142197561+YashVerma-251103@users.noreply.github.com>
2025-05-25 20:01:20 +0530

    changes to postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,9 @@
 // postcss.config.mjs
-import tailwindcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
+import tailwindcss from "@tailwindcss/postcss";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+import autoprefixer from "autoprefixer";
 
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer
-  ],
+  plugins: [tailwindcss, forms, typography, autoprefixer],
 };
