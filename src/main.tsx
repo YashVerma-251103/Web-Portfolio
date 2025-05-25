f04df1eb131160252376f5e8bb6e1f877bf51dff
Vemy <142197561+YashVerma-251103@users.noreply.github.com>
2025-05-25 21:09:58 +0530

    2

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// import './output.css'
 
 import App from './App' 
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+console.log('Main script executed')
+const rootElement = document.getElementById('root')
+console.log('Root element found:', rootElement)
+
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+  console.log('App rendered')
+}
